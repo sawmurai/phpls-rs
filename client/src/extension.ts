18945b11db19e29513ab5779a0fd7939808acc53
sawmurai <fabian.becker@b-it-d.de>
2020-12-19 10:41:57 +0100

    feat(client): add current copied version of ms sample client

---
+++ client/src/extension.ts
@@ -0,0 +1,85 @@
+import * as path from 'path';
+import { workspace, ExtensionContext, IndentAction, languages, window, Uri, TextDocument } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	Executable
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// Used with gratidude from Ben Robert Mewburn
+	// https://github.com/bmewburn/vscode-intelephense/blob/master/src/extension.ts
+	languages.setLanguageConfiguration('php', {
+		wordPattern: /(-?\d*\.\d\w*)|([^\-\`\~\!\@\#\%\^\&\*\(\)\=\+\[\{\]\}\|\;\:\'\"\,\.\<\>\/\?\s\\]+)/g,
+		onEnterRules: [
+			{
+				// e.g. /** | */
+				beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+				afterText: /^\s*\*\/$/,
+				action: { indentAction: IndentAction.IndentOutdent, appendText: ' * ' }
+			},
+			{
+				// e.g. /** ...|
+				beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+				action: { indentAction: IndentAction.None, appendText: ' * ' }
+			},
+			{
+				// e.g.  * ...|
+				beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+				action: { indentAction: IndentAction.None, appendText: '* ' }
+			},
+			{
+				// e.g.  */|
+				beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+				action: { indentAction: IndentAction.None, removeText: 1 }
+			},
+			{
+				// e.g.  *-----*/|
+				beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
+				action: { indentAction: IndentAction.None, removeText: 1 }
+			}
+		]
+	});
+
+	let serverModule = context.asAbsolutePath(
+		path.join('target', 'debug', 'phpls-rs')
+	);
+
+	const run: Executable = {
+		command: serverModule,
+		options: { cwd: "." },
+	};
+
+	let serverOptions: ServerOptions = {
+		run,
+		debug: run
+	};
+
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'php' }],
+		synchronize: {
+			fileEvents: workspace.createFileSystemWatcher('**/*.php')
+		},
+	};
+
+	client = new LanguageClient(
+		'phplsRs',
+		'PHP Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
