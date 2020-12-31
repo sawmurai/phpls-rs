a8571b5cb392ece8a1215c7ba733144f27ff0fba
Fabian Becker <fabian.becker@b-it-d.de>
2020-12-31 12:18:30 +0100

    feat(download): WIP client download

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,4 +1,8 @@
 import * as path from 'path';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
+
 import { workspace, ExtensionContext, IndentAction, languages, window, Uri, TextDocument } from 'vscode';
 
 import {
@@ -6,12 +10,53 @@ import {
 	LanguageClientOptions,
 	ServerOptions,
 	TransportKind,
-	Executable
+	Executable,
+	NotificationType
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
+  
+/**
+ * Downloads file from remote HTTP[S] host and puts its contents to the
+ * specified location.
+ */
+async function download(url, filePath) {
+  const proto = !url.charAt(4).localeCompare('s') ? https : http;
 
-export function activate(context: ExtensionContext) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(filePath);
+    let fileInfo = null;
+
+    const request = proto.get(url, response => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+        return;
+      }
+
+      fileInfo = {
+        mime: response.headers['content-type'],
+        size: parseInt(response.headers['content-length'], 10),
+      };
+
+      response.pipe(file);
+    });
+
+    // The destination stream is ended by the time it's called
+    file.on('finish', () => resolve(fileInfo));
+
+    request.on('error', err => {
+      fs.unlink(filePath, () => reject(err));
+    });
+
+    file.on('error', err => {
+      fs.unlink(filePath, () => reject(err));
+    });
+
+    request.end();
+  });
+}
+
+export async function activate(context: ExtensionContext) {
 	// Used with gratidude from Ben Robert Mewburn
 	// https://github.com/bmewburn/vscode-intelephense/blob/master/src/extension.ts
 	languages.setLanguageConfiguration('php', {
@@ -45,6 +90,23 @@ export function activate(context: ExtensionContext) {
 			}
 		]
 	});
+
+	let downloadPath = 'https://';
+	let storagePath = context.globalStoragePath;
+	let binary = storagePath + '/phplsrs-0.1.1';
+	if (!fs.existsSync(storagePath)) {
+		fs.mkdirSync(storagePath);
+	}
+
+	if (!fs.existsSync(binary)) {
+		
+		window.setStatusBarMessage("Downloading client ...");
+		await download(downloadPath, binary);
+		
+		window.setStatusBarMessage("Downloaded client");
+	} else {	
+		window.setStatusBarMessage("Client already installed.");
+	}
 
 	let serverModule = context.asAbsolutePath(
 		path.join('target', 'debug', 'phpls-rs')
